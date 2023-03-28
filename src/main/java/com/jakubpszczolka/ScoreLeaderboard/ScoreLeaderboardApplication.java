08e41fd167d4aae1ebabe2f408c1022c683c80f4
Jakub Pszczolka <jakub.pszczolka5@gmail.com>
2023-03-28 15:17:32 +0200

    Initial commit

---
+++ src/main/java/com/jakubpszczolka/ScoreLeaderboard/ScoreLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.jakubpszczolka.ScoreLeaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreLeaderboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubpszczolka/ScoreLeaderboard/ScoreLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubpszczolka.ScoreLeaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreLeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
